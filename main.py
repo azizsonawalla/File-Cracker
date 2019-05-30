0f5529020faddd5317b1756b0f335aa132c07a39
Aziz Sonawalla <aziz.sonawalla@sap.com>
2019-05-29 17:12:29 -0700

    Add a gitignore

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from controller import Controller
-
+"""
 config = {
 	"path": None,
+	"type": None,
 	"pass_min_length": None,
 	"pass_max_length": None,
 	"max_threads": None,
@@ -12,19 +13,25 @@ config = {
 	"special": True,
 	"possible": []
 }
+"""
 
-def capture_input():
-	print("\n\nWelcome to Excel Password Cracker\n\n")
+
+def capture_input() -> dict:
+	config = {}
+	supported_file_types = []  # TODO
+	print("\n\nWelcome to File Cracker\n\n")
 	while True:
-		print("Enter the path to the file you wish to crack:\n")
-		file_path = os.path.abspath(str(input()).replace("\"", ""))
+		file_path = os.path.abspath(str(input("Enter the path to the file you wish to crack:\n")).replace("\"", ""))
 		if not os.path.isfile(file_path):
 			print("\nHmmm...that doesn't look like a file. Try again:\n")
 			continue
 		filename, file_extension = os.path.splitext(file_path)
-		if file_extension not in SUPPORTED_EXTENSIONS:
+		if file_extension not in supported_file_types:
 			print("\nSorry, {} files are not supported. Try again:\n".format(file_extension))
 			continue
+		config["path"] = file_path
+		break
+	return config
 
 
 def main():
