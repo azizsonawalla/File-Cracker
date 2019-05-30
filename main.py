cf609db63a2eacb44ae42e2df58212f0d5da5a70
Aziz Sonawalla <aziz.sonawalla@sap.com>
2019-05-30 10:34:30 -0700

    Add more CLI interaction

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from controller import Controller
 config = {
 	"path": None,
 	"type": None,
-	"pass_min_length": None,
-	"pass_max_length": None,
+	"pass_min_len": None,
+	"pass_max_len": None,
 	"max_threads": None,
 	"lowercase": True,
 	"uppercase": True,
@@ -20,6 +20,8 @@ def capture_input() -> dict:
 	config = {}
 	supported_file_types = []  # TODO
 	print("\n\nWelcome to File Cracker\n\n")
+
+	# Get file location and validate
 	while True:
 		file_path = os.path.abspath(str(input("Enter the path to the file you wish to crack:\n")).replace("\"", ""))
 		if not os.path.isfile(file_path):
@@ -31,6 +33,17 @@ def capture_input() -> dict:
 			continue
 		config["path"] = file_path
 		break
+
+	print("Great! Now we'll need some information to help guess the password.\n")
+	print("The more details you provide, the faster we can crack the password.")
+
+	# Get password min length
+	while True:
+		pass_min_len = input("Minimum length of the passwords we should try: ")
+		try:
+			pass_min_len = int(pass_min_len)
+		except:
+			print("")
 	return config
 
 
